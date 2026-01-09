2197f40a05142ba7c9c36064cd280e5c957ce088
raymartinsajudarte <rayeleoterio08@gmail.com>
2026-01-08 22:28:50 -0300

    adição do exercicio 3

---
+++ exercicio03.py
@@ -0,0 +1,5 @@
+#Faça um Programa que peça dois números e imprima a soma.
+numero1 = int(input("Digite o primeiro numero: "))
+numero2 = int(input("Digite o segundo numero: "))
+soma = (numero1 + numero2)
+print("A soma entre os dois numeros é de: ", soma)
