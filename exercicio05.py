42377eaca1c2fbeb3f3922982cccf8b901011707
raymartinsajudarte <rayeleoterio08@gmail.com>
2026-01-10 23:19:36 -0300

    exercicio 5

---
+++ exercicio05.py
@@ -0,0 +1,4 @@
+#Digite um número em metros e converte em centímetros
+metros = float(input("Digite a metragem que você deseja converter em centimetros: "))
+centimetros = metros * 100
+print(f"{metros} metros equivalem a {centimetros:} centimetros.")
