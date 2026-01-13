d36ae503fe7d5a50fbc1ef636742975d112f3b0b
raymartinsajudarte <rayeleoterio08@gmail.com>
2026-01-12 23:13:42 -0300

    add exercicio 8

---
+++ exercicio08.py
@@ -0,0 +1,4 @@
+#Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Farenheit. farenheit = ((celsius / 5.0) * 9.0) + 32.0
+Celsius = float(input("Informe o valor em Celsius para converter em Farenheit: "))
+Farenheit = ((Celsius / 5.0) * 9.0) + 32.0
+print("A temperatura convertida de Celsius para Farenheit é: ", Farenheit)
